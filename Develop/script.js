73ad2667eeea6787bdcd4ff5782fe0b31d24bae9
Edward Reyes <reyes.edwardf@gmail.com>
2020-07-24 23:12:01 -0700

    created generatePassword() method

--- Develop/script.js
+++ Develop/script.js
@@ -3,11 +3,28 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  console.log("In generate password")
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
+}
+
+// Generate a password
+function generatePassword() {
+  var selectedCriteria = Number(prompt("[1] Choose password length\n" +
+  "[2] Choose lowercase, uppercase, numeric, and/or special characters\n" +
+  "Enter [1] or [2]"));
+  console.log(selectedCriteria);
+  console.log(typeof selectedCriteria)
+
+  if (selectedCriteria === 1) {
+    console.log("selectedCriteria is a number")
+  } else if (isNaN(selectedCriteria)) {
+    console.log("selectedCriteria is not a number")
+  }
+  
 }
 
 // Add event listener to generate button
