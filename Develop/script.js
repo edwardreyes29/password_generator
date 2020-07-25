78d24b02a5884f69646886dac694e58d48008fca
Edward Reyes <reyes.edwardf@gmail.com>
2020-07-25 03:41:36 -0700

    removed a console.log

--- Develop/script.js
+++ Develop/script.js
@@ -3,7 +3,6 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  console.log("In generate password")
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
