81d0a4597d3376ca0e404818f10f6b23dfd30dd2
Edward Reyes <reyes.edwardf@gmail.com>
2020-07-25 02:47:35 -0700

    created criteria objects for better organization

--- Develop/script.js
+++ Develop/script.js
@@ -25,11 +25,18 @@ function generatePassword() {
     }
     passwordLength = Number(input); // convert input into a number
   } while ((passwordLength < 8 || passwordLength > 128)); // if null, user clicked cancel
+
+  // create objects for each criteria
+  var lowercaseCond = {name: "lower", include: false, count: 0, id: 1};
+  var uppercaseCond = {name: "upper", include: false, count: 0, id: 2};
+  var numericCond = {name: "numeric", include: false, count: 0, id: 3};
+  var specialCond = {name: "special", include: false, count: 0, id: 4};
   
-  var includeLowerCase = false;  
-  var includeUpperCase = false;
-  var includeNumeric = false;
-  var includeSpecial = false;
+  // var includeLowerCase = false;  
+  // var includeUpperCase = false;
+  // var includeNumeric = false;
+  // var includeSpecial = false;
+
   do {
     // Notify the user to choose at least one criteria
     alert("\u2022 lowercase\n" + 
@@ -38,92 +45,94 @@ function generatePassword() {
     "\u2022 special characters\n\n" +
     "You must choose at least one criteria to include into your password")
     // Ask user if they want password to include lowercase characters
-    includeLowerCase = confirm("Do you want your password to include lowercase characters?");
-    includeUpperCase = confirm("Do you want your password to include uppercase characters?");
-    includeNumeric = confirm("Do you want your password to include numeric characters?");
-    includeSpecial= confirm("Do you want your password to include special characters?");
+    lowercaseCond.include = confirm("Do you want your password to include lowercase characters?");
+    uppercaseCond.include = confirm("Do you want your password to include uppercase characters?");
+    numericCond.include = confirm("Do you want your password to include numeric characters?");
+    specialCond.include = confirm("Do you want your password to include special characters?");
     
-  } while(!(includeLowerCase || includeUpperCase || includeNumeric || includeSpecial)); // if all are still false, loop again
+  } while(!(lowercaseCond.include || uppercaseCond.include || numericCond.include  || specialCond.include)); // if all are still false, loop again
 
   // Create arrays to store characters for special characters
   var specialChars = ['!', '\"', '#', '$', '%', '&','\'', '(', ')', '*', '+', ',', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '\\', '^', '{', '|', '}', '~', ' '];
   // var numbers = [1,2,3,4,5,6,7,8,8,9];
   // var lowercase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','']
 
+  
+
   criteriaArray = [];
   // include criteria numbers that are selected true into criteriaArray
-  if (includeLowerCase) {
-    criteriaArray.push(1);
+  if (lowercaseCond.include) {
+    criteriaArray.push(lowercaseCond);
   } 
-  if (includeUpperCase) {
-    criteriaArray.push(2);
+  if (uppercaseCond.include) {
+    criteriaArray.push(uppercaseCond);
   } 
-  if (includeNumeric) {
-    criteriaArray.push(3);
+  if (numericCond.include) {
+    criteriaArray.push(numericCond);
   } 
-  if (includeSpecial) {
-    criteriaArray.push(4);
+  if (specialCond.include) {
+    criteriaArray.push(specialCond);
   } 
   console.log(criteriaArray);
 
   // var res = String.fromCharCode(65);
   // console.log(res);
 
-  // Include count for each criteria to ensure that each one is met in password
-  var lowercaseCount = 0;
-  var uppercaseCount = 0;
-  var numericCount = 0;
-  var specialCount = 0;
-
   // Set a character limit for each criteria so each are guaranteed to be met in the password
-  var characterLimit = Math.floor(passwordLength / criteriaArray.length); 
-  console.log(characterLimit);
+  var characterLimit = Math.floor(passwordLength / criteriaArray.length);
 
   // Generate the password based on the given criteria
   var newPassword = ""; // to store the newly generated password
+  
   while (newPassword.length < passwordLength) {
     var randomInt = getRandomInt(criteriaArray.length);
 
-    var criteria = criteriaArray[randomInt];
-    // console.log(criteria);
+    var criteria = criteriaArray[randomInt].id;
+    console.log(criteria);
     console.log("generating...");
     switch (criteria) {
       case 1:
-        if (lowercaseCount < characterLimit) {
+        if (lowercaseCond.count < characterLimit) {
           // console.log("case 1");
           newPassword += 1;
-          lowercaseCount++;
+          lowercaseCond.count++;
         }
         break;
       case 2: 
-        if (uppercaseCount < characterLimit) {
+        if (uppercaseCond.count < characterLimit) {
           // console.log("case 2");
           newPassword += 2;
-          uppercaseCount++;
+          uppercaseCond.count++;
         }
         break;
       case 3:
-        if (numericCount < characterLimit) {
+        if (numericCond.count < characterLimit) {
           // console.log("case 3");
           newPassword += 3;
-          numericCount++;
+          numericCond.count++;
         }
         break;
       case 4:
-        if (specialCount < characterLimit) {
+        if (specialCond.count < characterLimit) {
           // console.log("case 4");
           newPassword += 4;
-          specialCount++;
+          specialCond.count++;
         }
         break;
     } // end switch
 
     // if all character limits are reached, reset all to zero
+    for (var i = 0; i < criteriaArray.length; i++) {
+      console.log("criteria met")
+    }
+
+
+
     if (lowercaseCount === uppercaseCount && uppercaseCount === numericCount && numericCount === specialCount) {
-      console.log("lower limit: " + lowercaseCount);
-      console.log("upper limit: " + uppercaseCount);
-      console.log("numeric limit: " + numericCount);
-      console.log("special limit: " + specialCount);
+      // console.log("lower limit: " + lowercaseCount);
+      // console.log("upper limit: " + uppercaseCount);
+      // console.log("numeric limit: " + numericCount);
+      // console.log("special limit: " + specialCount);
       lowercaseCount = 0;
       uppercaseCount = 0;
       numericCount = 0;
